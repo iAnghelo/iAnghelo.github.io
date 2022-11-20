5a643acadad2abaf42a9894698327f6843298ce3
iAnghelo <tanghelot@gmail.com>
2022-11-19 21:19:40 -0500

    holas

--- Javascript/slider.js
+++ Javascript/slider.js
@@ -1,29 +1,13 @@
-let slider = document.querySelector(".slider-contenedor")
-let sliderIndividual = document.querySelectorAll(".contenido-slider")
-let contador = 1;
-let width = sliderIndividual[0].clientWidth;
-let intervalo = 3000;
-
-window.addEventListener("resize", function(){
-    width = sliderIndividual[0].clientWidth;
-})
-
-setInterval(function(){
-    slides();
-},intervalo);
-
-
-
-function slides(){
-    slider.style.transform = "translate("+(-width*contador)+"px)";
-    slider.style.transition = "transform .8s";
-    contador++;
-
-    if(contador == sliderIndividual.length){
-        setTimeout(function(){
-            slider.style.transform = "translate(0px)";
-            slider.style.transition = "transform 0s";
-            contador=1;
-        },1500)
-    }
+const imgArray = [
+    '../Imagenes/slider1.jpg','../Imagenes/slider2.jpg','../Imagenes/slider7.jpg','../Imagenes/slider4.jpg','../Imagenes/slider5.jpg','../Imagenes/slider6.jpg'
+]
+const img = document.querySelector('#slide')
+//const img = document.getElementById('slide')
+let i = 0
+const slideShow = () => {
+    img.src = imgArray[i]
+    i = ( i < imgArray.length - 1) ? i+1 : 0
+}
+const interval = () => {
+    setInterval(slideShow, 1800)
 }
